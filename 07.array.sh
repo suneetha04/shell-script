12bbca75916dfb8affc18869f15ddf9efcea79b5
Suneetha Rani <suneetha04011622@gmail.com>
2025-01-02 20:52:50 +0530

    conversation added

---
+++ 07.array.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MOVIES=("Pushpa" "RRR" "Devara")
+
+echo "first movie: ${MOVIES[0]}"
+echo "second movie: ${MOVIES[1]}"
+echo "third  movie: ${MOVIES[2]}"
+echo "all movies are: ${MOVIES[@]}"
+
