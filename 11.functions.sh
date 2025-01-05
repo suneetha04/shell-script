d8821dd8a23d2db8f94f43ae56071b7983aff1a0
Suneetha Rani <suneetha04011622@gmail.com>
2025-01-05 10:16:42 +0530

    installation process

---
+++ 11.functions.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+USERID=$(id -u)
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+       echo "$2...FAILURE"
+       exit1
+    else
+       echo "$2...SUCCESS"
+    fi
+}
+
+if [ $USERID -ne 0 ]
+then
+   echo "ERROR: you must have sudo access for this execution"
+   exit1 #other than 0
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+then # not installed
+   dnf install mysql -y
+   VALIDATE $? "Installing MySQL"
+else
+   echo "MySQL is already installed"
+fi
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+   dnf install git -y
+   VALIDATE $? "Installing GIT"
+else
+   echo "GIT is already installed"
+fi
